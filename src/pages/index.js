77716491fdeb5f0a65806eff5960141e52d83fc7
Santiago Manuel Rodriguez <santiagomanuelrodriguez@gmail.com>
2022-08-06 18:42:46 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Layout from "../components/layout";;
 import Layout from "../components/layout";
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Key Gatsby Concept:
 // Gatsby automatically creates pages for components that are the default exports of files in the src/pages directory. 
@@ -11,6 +11,15 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <br/>
+      <StaticImage
+        alt="Kobe Bryant shirt"
+        src="../images/kobe-shirt.jpg"
+      />
     </Layout>
   );
 };
